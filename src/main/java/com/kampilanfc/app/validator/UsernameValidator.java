459f31888161ff071b26e5c05b6746c81b08d8bd
john-verana <johnverana@gmail.com>
2025-10-22 02:43:03 +0800

    feat(validation): implementUsernameValidator to handle username pattern validation using ValidationResult

---
+++ src/main/java/com/kampilanfc/app/validator/UsernameValidator.java
@@ -0,0 +1,33 @@
+package com.kampilanfc.app.validator;
+
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Pattern;
+
+@Component
+public class UsernameValidator {
+
+    private static final int MIN_LENGTH = 3;
+    private static final int MAX_LENGTH = 20;
+    private static final String USERNAME_PATTERN = "^[a-zA-Z0-9._]+$";
+
+    private static final Pattern PATTERN = Pattern.compile(USERNAME_PATTERN);
+
+    public ValidationResult validate(String username) {
+        if (username == null || username.isBlank())
+            return ValidationResult.failure("Username is required");
+
+        if (username.length() < MIN_LENGTH) {
+            return ValidationResult.failure("Username too short (minimum 3 characters)");
+        }
+
+        if (username.length() > MAX_LENGTH) {
+            return ValidationResult.failure("Username too long (maxium 20 characters)");
+        }
+
+        if (!PATTERN.matcher(username).matches()) {
+            return ValidationResult.failure("Username can only contain letters, numbers, dots, and underscores");
+        }
+        return ValidationResult.success();
+    }
+}
